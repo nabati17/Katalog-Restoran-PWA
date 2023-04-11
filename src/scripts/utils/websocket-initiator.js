7d0a883a3ac032ed94381416aea4db3d7057f62c
nabati17 <asnaba93@gmail.com>
2023-04-11 09:58:17 +0700

    fix service worker errors

--- src/scripts/utils/websocket-initiator.js
+++ src/scripts/utils/websocket-initiator.js
@@ -1,26 +1,3 @@
-// import NotificationHelper from './notification-helper';
-// import CONFIG from '../globals/config';
-
-// const WebSocketInitiator = {
-//   init(url) {
-//     const webSocket = new WebSocket(url);
-//     webSocket.onmessage = this._onMessageHandler;
-//   },
-
-//   _onMessageHandler(message) {
-//     const restaurant = JSON.parse(message.data);
-
-//     NotificationHelper.sendNotification({
-//       title: `${restaurant.name} is on Ready!`,
-//       options: {
-//         body: restaurant.customerReviews,
-//         image: `${CONFIG.BASE_IMAGE_URL + restaurant.pictureId}`,
-//       },
-//     });
-//   },
-// };
-
-// export default WebSocketInitiator;
 import NotificationHelper from './notification-helper';
 import CONFIG from '../globals/config';
 
